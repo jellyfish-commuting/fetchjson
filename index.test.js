b0915a082520cf7df9357756b8a6822d8d201cfa
thejellyfish <the@jellyfish-commuting.com>
2020-09-05 22:40:24 +0200

    Use require - Add dummy test

--- index.test.js
+++ index.test.js
@@ -1,19 +1,18 @@
-// import fetchjson from './index'
-// global.fetch = require('node-fetch');
+const fetchjson = require('./index');
+global.fetch = require('node-fetch');
 
 // Start tests ...
 describe('fetchjson', () => {
   // Fetch data successfully
   it('expect JSON while fetching public API', async () => {
-    /*
-    const result = await fetchjson('GET', 'https://randomuser.me/api/', { results: 1 });
+    const result = await fetchjson('https://randomuser.me/api/', { results: 1 });
 
     expect(result).toMatchObject(
       expect.objectContaining({
         results: expect.any(Array),
       })
     );
-    */
+
     expect("Boom !").toBe("Boom !");
   });
 });
